54bdde7eed5cfd63cff04df001756b56a6bf5ff8
rizry <rizzwan.ansarii@gmail.com>
2022-08-27 14:05:22 +0530

    added welcome message

---
+++ LineComparison.java
@@ -0,0 +1,9 @@
+package com.java.day3;
+
+public class LineComparison {
+
+  public static void main(String[] args) {
+    System.out.println("--Welcome to Line Comparison Computation Program--\n");
+
+  }
+}
